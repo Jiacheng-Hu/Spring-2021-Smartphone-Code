9a9ff71b6afe7f8a1a2dadeaacc7a11e62ce126a
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2021-02-15 14:07:40 +0800

    add files

---
+++ TableViewXib/TableViewXib/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  TableViewXib
+//
+//  Created by 胡嘉诚 on 2021/2/15.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var lblCities: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ TableViewXib/TableViewXib/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  TableViewXib
+//
+//  Created by 胡嘉诚 on 2021/2/15.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let cities:[String] = ["Seattle","Portland", "San Francisco", "New York"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return cities.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("TableViewCell", owner: self, options: nil)?.first as! TableViewCell
+        cell.lblCities.text = cities[indexPath.row]
+        return cell
+    }
+
+}
