b1311549d75405b41e9ce214b2454ef4fe887b55
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2021-02-17 17:11:24 +0800

    add labels

--- AlertControllerExample/AlertControllerExample/ViewController.swift
+++ AlertControllerExample/AlertControllerExample/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var lblText: UILabel!
+    
+    var globalTxtField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblText.text = "Please click the button"
     }
 
     @IBAction func actionController(_ sender: UIButton) {
@@ -20,6 +25,7 @@ class ViewController: UIViewController {
         
         let ok = UIAlertAction(title: "Ok", style: .default) { (actionController) in
             print("Ok")
+            self.lblText.text = self.globalTxtField?.text
         }
         
         let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (actionController) in
@@ -28,6 +34,11 @@ class ViewController: UIViewController {
         
         alert.addAction(ok)
         alert.addAction(cancel)
+        alert.addTextField { (txtField) in
+            txtField.placeholder = "Type Something"
+            self.globalTxtField = txtField
+        }
+        
         self.present(alert, animated: true, completion: nil)
     }
     
