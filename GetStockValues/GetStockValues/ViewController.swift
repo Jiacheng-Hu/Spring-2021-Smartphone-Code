0de5455563f8ef36594382ad4c84d91e3a549aa5
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2021-02-24 23:09:50 +0800

    add stock value alert

--- GetStockValues/GetStockValues/ViewController.swift
+++ GetStockValues/GetStockValues/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import SwiftyJSON
+import SwiftSpinner
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -21,13 +24,14 @@ class ViewController: UIViewController {
     
     @IBAction func alertAction(_ sender: UIButton) {
         
-        let alert = UIAlertController(title: "Stock Sample", message: "Show Stock Value", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Get Stock Price", message: "Type in The Symbol", preferredStyle: .alert)
         let ok = UIAlertAction(title: "Ok", style: .default) { (alertAction) in
-            self.lblStockValue.text = self.globalStockTxtField?.text
-            print("ok")
+//            self.lblStockValue.text = self.globalStockTxtField?.text
+            guard let stock = self.globalStockTxtField?.text else {return}
+            self.getStockValue(stock)
         }
         let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (alertAction) in
-            print("cancel")
+            
         }
         
         alert.addTextField { (stockTxtField) in
@@ -37,6 +41,35 @@ class ViewController: UIViewController {
         alert.addAction(ok)
         alert.addAction(cancel)
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getStockValue(_ stockSymbol: String) {
+        let url = getURL(stockSymbol)
+//        print(url)
+        SwiftSpinner.show("Getting \(stockSymbol) value")
+        AF.request(url).responseJSON { response in
+            SwiftSpinner.hide()
+            if response.error == nil {
+                guard let data = response.data else {return}
+                let stockData: JSON = JSON(data)
+//                print(stockData)
+                guard let stocks = stockData.array else{return}
+                if stocks.count == 0{
+                    self.lblStockValue.text = "\(stockSymbol) does not exist"
+                }else{
+                    for stock in stocks {
+                        self.lblStockValue.text = "\(stock["symbol"].stringValue) : $\(stock["price"].floatValue)"
+                    }
+                }
+                
+            }else{
+                print(response.error?.localizedDescription ?? "error")
+            }
+        }
+    }
+    
+    func getURL(_ stockSymbol: String) -> String{
+        return apiURL + stockSymbol + "?apikey=" + apiKey
     }
     
 
