374a8db4627d2c0075f70ef40914bd484566c203
Jiacheng-Hu <hu.jiac@husky.neu.edu>
2021-03-31 22:55:34 +0800

    commit changes

--- LoginProject/LoginProject/ViewController.swift
+++ LoginProject/LoginProject/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var lblStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("test")
     }
+    
+    @IBAction func btnLogin(_ sender: Any) {
+//        let email = txtEmail.text!
+//        let password = txtPassword.text!
+//
+//        if email.count == 0 || password.count < 6 {
+//            lblStatus.text = "Please enter valid email or password"
+//        }
+//
+//        if !email.isValidEmail {
+//            lblStatus.text = "Please enter valid email or password"
+//        }
+//
+//        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+//
+//            if error != nil {
+//                self.lblStatus.text = error?.localizedDescription
+//                return
+//            }
+//
+//        }
+        
+        performSegue(withIdentifier: "LoginSegue", sender: self)
+    }
+    
 
 
 }
